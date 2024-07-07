539007dff4f99ce08e809d018300cc2fa4bad6ca
Lucas Teixeira <lucasteixeira8784@gmail.com>
2024-07-07 15:37:40 -0300

    tratamento de erros

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,7 +1,10 @@
 package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,7 +17,19 @@ public class TratadorDeErros {
     //Criamos um método para tratar um erro específico sinalizado pelo comentário.
     //Podemos criar quantos ExceptionHandlers forem necessários.
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErros(){
+    public ResponseEntity tratarErros404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErros400(MethodArgumentNotValidException ex){
+        var erro = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erro.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
